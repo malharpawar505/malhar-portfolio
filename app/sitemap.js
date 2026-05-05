3e0596629a2c18658b1ae14c40b757e63e0e6b54
Malhar Pawar <pawarmalhar505@gmail.com>
2026-05-05 16:12:25 +0530

    feat: add sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,30 @@
+export default function sitemap() {
+  const baseUrl = "https://malharmpawar.com";
+
+  return [
+    {
+      url: `${baseUrl}/`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/projects`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/blog`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.7,
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+  ];
+}
